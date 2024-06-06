e737f9fdbd63ca5c243a5adbb2a4f282c0b18206
ai-null <ainul.y9b@gmail.com>
2024-06-06 07:42:51 +0700

    update: add migration table book creation

---
+++ database/migrations/2024_06_06_003230_create_book_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('title');
+            $table->string('author');
+            $table->integer('stock')->default(0);
+            $table->string('cover');
+            $table->string('description')->nullable();
+            $table->string('ISBN')->nullable();
+            $table->string('publisher')->nullable();
+            $table->string('language')->nullable()->default('Indonesia');
+            $table->integer('publishing_year');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('book');
+    }
+};
