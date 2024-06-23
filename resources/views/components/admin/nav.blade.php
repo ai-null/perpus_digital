f0373ed368a271c35469a074e47d9c864817c5be
ai-null <ainul.y9b@gmail.com>
2024-06-23 21:48:39 +0700

    update: remove unused subtree in navbar

--- resources/views/components/admin/nav.blade.php
+++ resources/views/components/admin/nav.blade.php
@@ -184,60 +184,6 @@
                     </ul>
                 </li>
 
-
-                {{-- WIDGET WITH BADGE --}}
-                <li class="nav-header">EXAMPLES</li>
-                <li class="nav-item"> <a href="#" class="nav-link"> <i
-                            class="nav-icon bi bi-box-arrow-in-right"></i>
-                        <p>
-                            Auth
-                            <i class="nav-arrow bi bi-chevron-right"></i>
-                        </p>
-                    </a>
-                    <ul class="nav nav-treeview">
-                        <li class="nav-item"> <a href="#" class="nav-link"> <i
-                                    class="nav-icon bi bi-box-arrow-in-right"></i>
-                                <p>
-                                    Version 1
-                                    <i class="nav-arrow bi bi-chevron-right"></i>
-                                </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item"> <a href="./examples/login.html" class="nav-link"> <i
-                                            class="nav-icon bi bi-circle"></i>
-                                        <p>Login</p>
-                                    </a> </li>
-                                <li class="nav-item"> <a href="./examples/register.html" class="nav-link"> <i
-                                            class="nav-icon bi bi-circle"></i>
-                                        <p>Register</p>
-                                    </a> </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item"> <a href="#" class="nav-link"> <i
-                                    class="nav-icon bi bi-box-arrow-in-right"></i>
-                                <p>
-                                    Version 2
-                                    <i class="nav-arrow bi bi-chevron-right"></i>
-                                </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item"> <a href="./examples/login-v2.html" class="nav-link"> <i
-                                            class="nav-icon bi bi-circle"></i>
-                                        <p>Login</p>
-                                    </a> </li>
-                                <li class="nav-item"> <a href="./examples/register-v2.html" class="nav-link">
-                                        <i class="nav-icon bi bi-circle"></i>
-                                        <p>Register</p>
-                                    </a> </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item"> <a href="./examples/lockscreen.html" class="nav-link"> <i
-                                    class="nav-icon bi bi-circle"></i>
-                                <p>Lockscreen</p>
-                            </a> </li>
-                    </ul>
-                </li>
-
             </ul>
             <!--end::Sidebar Menu-->
         </nav>
